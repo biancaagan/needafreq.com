14b46754dcbe544c75de5fcc2ef1dd334cdd1e21
biancaagan <biancaagan@gmail.com>
2023-03-23 16:37:35 -0400

    update

--- sketch.js
+++ sketch.js
@@ -13,7 +13,6 @@ let nums2 = [4, 5, 6, 4, 5, 6];
 let nums3 = [2, 5, 7, 0, 2, 5, 7, 0];
 
 
-
 let randomNum;
 let n1 = 0;
 let n2 = 0;
@@ -24,16 +23,25 @@ let n6 = 0;
 
 let count = 0;
 
+let blockW = 80;
+let blockH = 100;
+
 function setup() {
-  createCanvas(300, 200);
+  createCanvas(600, 400);
   
   start = createButton('Spin');
   start.mouseClicked(() => startB = true);
-  start.position(10, 10);
+  start.position(width/2 - 110, 20);
+  start.size(100, 60);
+  start.style("font-family", "Comic Sans MS");
+  start.style("font-size", "30px");
   
   stop = createButton('Stop');
   stop.mouseClicked(() => stopB = true);
-  stop.position(60, 10);
+  stop.position(width/2 + 10, 20);
+  stop.size(100, 60);
+  stop.style("font-family", "Comic Sans MS");
+  stop.style("font-size", "30px");
     
   randomNum1 = random(nums2);
   randomNum2 = random(nums1);
@@ -48,11 +56,13 @@ function draw() {
   background(220);
   
   fill(0);
+  textFont('Comic Sans MS');
+  textSize(50);
   
   // Spin label
   if(startB == false && stopB == false){
     let num = 'Spin to shuffle';
-    text(num, width/2, 150);
+    text(num, width/2, 300);
   } 
   
   if(startB == true){
@@ -63,7 +73,7 @@ function draw() {
     n5 = random(nums3);
     n6 = random(nums);
     num = 'Stop for number';
-    text(num, width/2, 150);
+    text(num, width/2, 300);
     
     randomNum1 = random(nums2);
     randomNum2 = random(nums1);
@@ -85,41 +95,42 @@ function draw() {
   
 
   textAlign(CENTER);
-  textSize(22);
+  textSize(80);
   
   // Number blocks
   noFill();
-  rect(width/2 - 105, height/2 - 35, 30, 40, 10);
+  rect(width/2 - 265, height/3, blockW, blockH, 10);
   fill(0);
-  text(n1, width/2 - 90, height/2 - 8);
+  text(n1, width/2 - 225, height/2 + 12);
   
   noFill();
-  rect(width/2 - 70, height/2 - 35, 30, 40, 10);
+  rect(width/2 - 180, height/3, blockW, blockH, 10);
   fill(0);
-  text(n2, width/2 - 55, height/2 - 8);
+  text(n2, width/2 - 140, height/2 + 12);
   
   noFill();
-  rect(width/2 - 35, height/2 - 35, 30, 40, 10);
+  rect(width/2 - 95, height/3, blockW, blockH, 10);
   fill(0);
-  text(n3, width/2 - 20, height/2 - 8);
+  text(n3, width/2 - 55, height/2 + 12);
   
   // dot
   fill(0);
-  ellipse(width/2, height/2, 4, 4);
+  ellipse(width/2, height/2 + 25, 10, 10);
   fill(0);
   
+  // Number blocks
   noFill();
-  rect(width/2 + 5, height/2 - 35, 30, 40, 10);
+  rect(width/2 + 15, height/3, blockW, blockH, 10);
   fill(0);
-  text(n4, width/2 + 20, height/2 - 8);
+  text(n4, width/2 + 55, height/2 + 12);
   
   noFill();
-  rect(width/2 + 40, height/2 - 35, 30, 40, 10);
+  rect(width/2 + 100, height/3, blockW, blockH, 10);
   fill(0);
-  text(n5, width/2 + 55, height/2 - 8);
+  text(n5, width/2 + 140, height/2 + 12);
   
   noFill();
-  rect(width/2 + 75, height/2 - 35, 30, 40, 10);
+  rect(width/2 + 185, height/3, blockW, blockH, 10);
   fill(0);
-  text(n6, width/2 + 90, height/2 - 8);
+  text(n6, width/2 + 225, height/2 + 12);
 }
